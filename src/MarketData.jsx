722fa136f2fd547d6878bbb3c51f295c8b9318f9
nazimdalal360-coder <nazimdalal360@gmail.com>
2026-04-07 02:33:56 +0530

    Update MarketData.jsx

--- src/MarketData.jsx
+++ src/MarketData.jsx
@@ -1,32 +1,5 @@
-import { useEffect, useState } from "react";
-
 function MarketData() {
-  const [price, setPrice] = useState("Loading...");
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await fetch("https://query1.finance.yahoo.com/v8/finance/chart/^NSEI");
-        const data = await res.json();
-        const price = data.chart.result[0].meta.regularMarketPrice;
-        setPrice(price);
-      } catch (err) {
-        setPrice("Error");
-      }
-    }
-
-    fetchData();
-    const interval = setInterval(fetchData, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <div>
-      <h2>NIFTY 50</h2>
-      <h1>{price}</h1>
-    </div>
-  );
+  return <h1>TEST HO RAHA HAI 🔥</h1>;
 }
 
 export default MarketData;

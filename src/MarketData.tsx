2739411e4843b8f20de29a4de9b0c62b03b18fa2
nazimdalal360-coder <nazimdalal360@gmail.com>
2026-04-08 16:30:41 +0530

    Update MarketData.tsx

--- src/MarketData.tsx
+++ src/MarketData.tsx
@@ -1,26 +1,25 @@
 import { useEffect, useState } from "react";
 
 export default function MarketData() {
-  const [price, setPrice] = useState(null);
+  const [price, setPrice] = useState<number | null>(null);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await fetch("https://api.allorigins.win/raw?url=https://query1.finance.yahoo.com/v8/finance/chart/%5ENSEI");
+        const res = await fetch("/.netlify/functions/getNifty");
         const data = await res.json();
-
-        const livePrice = data.chart.result[0].meta.regularMarketPrice;
-        setPrice(livePrice);
-      } catch (error) {
-        console.error("Error fetching data:", error);
+        setPrice(data.price);
+      } catch (err) {
+        console.error(err);
       }
     }
 
     fetchData();
+    setInterval(fetchData, 5000);
   }, []);
 
   return (
-    <div style={{color: "white", marginBottom: "10px"}}>
+    <div>
       <h2>NIFTY LIVE</h2>
       <p>{price ? price : "Loading..."}</p>
     </div>
